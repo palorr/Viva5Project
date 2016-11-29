6d19b2d10c75a73760830356f3c50976aa546a2e
Orestis Meikopoulos <diktyas1988@gmail.com>
2016-11-29 16:52:30 +0200

    Added SignalR Project to Source Control

---
+++ VivaWallet.Server.Web.Api/SignalRChat/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using Serilog;
+using Microsoft.AspNet.SignalR.Hubs;
+using SignalRChat.Models;
+
+namespace SignalRChat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(ChatMessage chatMessage)
+        {
+            Clients.All.SendMessage(chatMessage);
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+ 
+}
